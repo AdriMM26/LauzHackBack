23231d1519737f5d0004ae2733c38b6b46e30109
AdriMM26 <adria.martinez@hackupc.com>
2022-12-04 02:49:08 +0100

    Afegit Back beta

---
+++ flaskProject1/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request, json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return "Hello World!"
+
+
+@app.route('/api/image', methods=['POST'])
+def getImage():
+    import base64
+
+    if request.data == None:
+        return jsonify({'message': 'ERROR while getting image'})
+    else:
+        bodyParsed = json.loads(request.data)
+        with open("imageToSave.png", "wb") as fh:
+            fh.write(base64.urlsafe_b64decode(bodyParsed["image"]))
+            return "Done"
+
+
+if __name__ == '__main__':
+    app.run()
